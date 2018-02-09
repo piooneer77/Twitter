747b367069a7ab82ff39a1f4ec5c08ad0cfee079
Piooneer77 <piooneer77@gmail.com>
2018-02-09 06:51:25 +0200

    first coloured complete schema

--- Twitter/Views/TweetCell.swift
+++ Twitter/Views/TweetCell.swift
@@ -15,8 +15,20 @@ class TweetCell: UICollectionViewCell {
     }
     
     private func setViewsConstraints(){
-        setLabelConstraints(label: tweetLabel)
+        addSubViewsInsideMainViews()
+        setTweetLabelConstraints(label: tweetLabel)
         setImageConstraints(image: imageView)
+        setUsernameLabelConstraints(label: usernameLabel)
+        setBioTextViewConstraints(text: bioTextView)
+        setFollowButtonConstraints(button: followButton)
+    }
+    
+    private func addSubViewsInsideMainViews(){
+        addSubview(imageView)
+        addSubview(tweetLabel)
+        addSubview(usernameLabel)
+        addSubview(bioTextView)
+        addSubview(followButton)
     }
     
     private let imageView : UIImageView = {
@@ -34,8 +46,31 @@ class TweetCell: UICollectionViewCell {
         return label
     }()
     
+    private let usernameLabel : UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "username"
+        label.backgroundColor = UIColor.yellow
+        return label
+    }()
+    
+    private let bioTextView : UITextView = {
+        let text = UITextView()
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.text = "bio\nText\nNew Text"
+        text.backgroundColor = UIColor.green
+        return text
+    }()
+    
+    private let followButton : UIButton = {
+       let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Follow", for: .normal)
+        button.backgroundColor = .brown
+        return button
+    }()
+    
     private func setImageConstraints(image : UIImageView){
-        addSubview(image)
         NSLayoutConstraint.activate([
             image.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
             image.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12),
@@ -44,13 +79,39 @@ class TweetCell: UICollectionViewCell {
         ])
     }
     
-    private func setLabelConstraints(label : UILabel){
-        addSubview(tweetLabel)
+    private func setTweetLabelConstraints(label : UILabel){
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: self.topAnchor, constant: 15),
-            label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 70),
-            label.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15),
+            label.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            label.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 12),
+            label.trailingAnchor.constraint(equalTo: followButton.leadingAnchor, constant: -12),
             label.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    private func setUsernameLabelConstraints(label : UILabel){
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: tweetLabel.bottomAnchor, constant: 12),
+            label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 70),
+            label.trailingAnchor.constraint(equalTo: followButton.leadingAnchor, constant: -12),
+            label.heightAnchor.constraint(equalToConstant: 30)
+            ])
+    }
+    
+    private func setBioTextViewConstraints(text : UITextView){
+        NSLayoutConstraint.activate([
+            text.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 12),
+            text.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 12),
+            text.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -12),
+            text.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12)
+        ])
+    }
+    
+    private func setFollowButtonConstraints(button : UIButton){
+        NSLayoutConstraint.activate([
+            button.heightAnchor.constraint(equalToConstant: 60),
+            button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -12),
+            button.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            button.widthAnchor.constraint(equalToConstant: 110)
         ])
     }
     
